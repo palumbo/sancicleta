a56a31b515ee9393159d8ba45ec442991c7b9534
Palumbo <jhpalumbo@gmail.com>
2014-05-23 05:57:31 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML5>
+<html> 
+
+<head> 
+	<title>Sancicleta.com - San Antonio's Bicycling Home</title> 
+	<style type="text/css"> 
+	
+		body { 
+			font-family: sans-serif; 
+			background-color: aqua; 
+		}
+		
+	</style> 
+	
+</head> 
+
+<body> 
+
+<h1>Sancicleta.com</h1>
+<p>
+A community-driven, comprehensive list of San Antonio's bicycle rides. 
+</p>
+
+
+
+
+
+</body> 
+</html> 
